9fbd57d20b9f75fa2f83810cab18051f2b325133
ZouaouiK <73880704+ZouaouiK@users.noreply.github.com>
2021-09-28 09:40:17 +0100

    dynamic input asset

--- src/SwapPortfolio.js
+++ src/SwapPortfolio.js
@@ -2,21 +2,54 @@ import React, { useEffect, useMemo, useState } from 'react';
 
 
 function SwapPortfolio() {
+    const [values, setValues] = useState();
 
+    function createInputs(n) {
+        let ret=[];
+        console.log("createInput nb =",n);
+        for (var i = 1; i <= n; i++) {
+           ret.push(
+               <>
+            <div key={i}>
+                <input type="text" />
+                <input type='button' value={`create Asset${i}`} />
+                
+            </div>
+            <br/>
+            </>
+            )
+        };
+        return ret;
+
+    }
+    useEffect(() => {
+        createInputs(values)
+    }, [values]);
+    
+  
+    function addInputs(event) {
+        let val = event.target.value;
+        console.log("nb asset ",val);
+        setValues(val);
+      //  createInputs(val);
+    }
 
     return (
         <div className="App">
             <h1> Swap Portfolio : </h1>
-            <br/>
-            <br/>
+            <br />
+            <br />
 
-            Numbre des Assets : <input type="number" min="1" max="10"></input><br/>
-            <br/>
-           Amount : <input type="number" ></input><br/>
-           <br/>
+            Numbre des Assets : <input type="number" min="1" max="10" value={values} onChange={addInputs}></input><br />
+            <br />
+            {createInputs(values)}
+            Amount : <input type="number" ></input><br />
+            <br />
 
-           <button className="btn btn-primary">Swap </button>
+            <button className="btn btn-primary">Swap </button>
+            <br />
 
+           
         </div>
 
     )
